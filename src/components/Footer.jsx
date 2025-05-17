a34e3901d45f8ac5b44d25aa6b349b001cdddd5a
Sumanth S R <134716473+Sumanthsrgowda@users.noreply.github.com>
2025-05-17 19:21:42 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Footer = ({ darkMode, setDarkMode }) => {
   return (
-    <div className="footer-format">
-    <footer className={`navbar navbar-expand-lg ${darkMode ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`}>
+    <footer className={`navbar ${darkMode ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`}>
       <div className="navbar-brand footer-center">© 2025 SoftSell. All rights reserved.</div>
     </footer>
-    </div>
   );
 };
 
